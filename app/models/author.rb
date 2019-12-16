496f1cf552c7912d6f338fe4aaf361897d9cc858
Rachel Lew <rlew07@gmail.com>
2019-12-15 19:52:43 -0700

    Add average_page_count method

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,4 +2,8 @@ class Author < ApplicationRecord
   validates_presence_of :name
   has_many :author_books
   has_many :books, through: :author_books
+
+  def average_page_count
+    books.average(:pages)
+  end
 end
